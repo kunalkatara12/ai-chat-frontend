a77783649b8eba787e1b6495b27ed977eb3073cf
kunalkatara12 <kunalkatara888@gmail.com>
2023-12-30 13:34:15 +0530

    vite proxy removed

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy:{
-      '/':'https://ai-chatbackend.onrender.com/api/v1',
-    }
-  },
+  // server: {
+  //   proxy:{
+  //     '/':'https://ai-chatbackend.onrender.com/api/v1',
+  //   }
+  // },
   plugins: [react()],
 })
